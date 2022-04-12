b316025f7c4b72130100fc283e16cec894725f23
WessalJawad95 <wessaljawad95@gmail.com>
2022-04-13 01:44:21 +0300

    create navbar relates #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <News />
+
     </div>
   );
 }
