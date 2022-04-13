23a038a3ae4831b7ffa4c34e99932428722cb907
mayar-kabaja <kabajamayar@gmail.com>
2022-04-13 17:05:16 +0300

    edit package json Relates #19

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://bing-news-search1.p.rapidapi.com/news',
+  params: { safeSearch: 'Off', textFormat: 'Raw' },
+  headers: {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': '9eeb5e7ab5msh6d838c238f409a7p1b103cjsn724c693ca437',
+  },
+};
+
+axios
+  .request(options)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 function App() {
   return (
     <div className="App">
