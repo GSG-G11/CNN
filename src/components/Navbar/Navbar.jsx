e4f204a4e9f55bab244cf172b979a78ee08c1955
mayar-kabaja <kabajamayar@gmail.com>
2022-04-13 14:54:56 +0300

    create loading component Relates #17

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <div className="header">
       <img src="https://logos-world.net/wp-content/uploads/2020/11/CNN-Logo.png" alt="logo" />
+      <a href="#news" className="link-news">News</a>
     </div>
   );
 }
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import Card from '../Card/Card';
+import Loading from '../Loading/Loading';
 import './News.css';
 
 function News() {
   const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const filterData = data.filter(
+    (obj) => obj.title.includes(search) || obj.title.includes(search[0].toUpperCase()),
+  );
   useEffect(() => {
     const abortController = new AbortController();
-    fetch('https://newsapi.org/v2/everything?q=Apple&from=2022-04-12&sortBy=popularity&apiKey=12e941912db6470582ba9b98234b84f2', { signal: abortController.signal })
+    fetch(
+      'https://newsapi.org/v2/everything?q=Apple&from=2022-04-12&sortBy=popularity&apiKey=12e941912db6470582ba9b98234b84f2',
+      { signal: abortController.signal },
+    )
       .then((res) => res.json())
-      .then((Data) => setData(Data.articles))
+      .then((Data) => {
+        setData(Data.articles);
+        setLoading(false);
+      })
       .catch(() => toast.error('Server Erorr !'));
     return () => {
       abortController.abort();
     };
   }, []);
   return (
-    <div className="news">
+    <div className="news" id="news">
       <h2> News </h2>
       <input
         type="search"
@@ -25,13 +36,13 @@ function News() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <div className="Cards">
-        {
-          data.filter((obj) => (obj.title.includes(search)
-          || obj.title.includes(search[0].toUpperCase())))
-            .map((obj) => <Card obj={obj} />)
-        }
-      </div>
+      {!loading ? (
+        <div className="Cards">
+          {filterData.length ? filterData.map((obj) => <Card obj={obj} />) : <p>No resultes</p>}
+        </div>
+      ) : (
+        <Loading />
+      )}
       <ToastContainer />
     </div>
   );
