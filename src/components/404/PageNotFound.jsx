b674cf71ddfade77f1a4bacc70aa558b9c471329
mayar-kabaja <kabajamayar@gmail.com>
2022-04-13 23:17:43 +0300

    page not found Relates #28

---
+++ src/components/404/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PageNotFound.css';
+
+function PageNotFound() {
+  return (
+    <div>
+      <img className="PageNotFound" src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png" alt="" />
+    </div>
+  );
+}
+export default PageNotFound;
